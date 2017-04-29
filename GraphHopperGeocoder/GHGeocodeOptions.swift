2757594d882fa930d6e52b0c1293d73b2cf01ee5
Phil Schilter <phil.schilter@gmail.com>
2017-04-29 16:05:01 +0200

    :hammer: add additional initializers for point and location

--- GraphHopperGeocoder/GHGeocodeOptions.swift
+++ GraphHopperGeocoder/GHGeocodeOptions.swift
@@ -6,19 +6,35 @@ open class GeocodeOptions: NSObject {
     public var locale = "en"
     public var limit = 10
     public var debug = false
+    public var point: CLLocationCoordinate2D?
+    public var provider = "default"
 
     public init(_ query: String) {
         assert(!query.isEmpty, "Specify a query.")
         self.query = query
     }
 
+    public init(_ query: String, point: CLLocationCoordinate2D) {
+        self.query = query
+        self.point = point
+    }
+
+    public convenience init(_ query: String, location: CLLocation) {
+        self.init(query, point: location.coordinate)
+    }
+
     internal var params: [URLQueryItem] {
-        let params: [URLQueryItem] = [
+        var params: [URLQueryItem] = [
             URLQueryItem(name: "q", value: String(query)),
             URLQueryItem(name: "locale", value: locale),
             URLQueryItem(name: "limit", value: String(limit)),
-            URLQueryItem(name: "debug", value: String(debug))
+            URLQueryItem(name: "debug", value: String(debug)),
+            URLQueryItem(name: "provider", value: provider)
         ]
+
+        if let point = point {
+            params.append(URLQueryItem(name: "point", value: "\(String(point.latitude)),\(String(point.longitude))"))
+        }
 
         return params
     }
--- GraphHopperGeocoderExample/ViewController.swift
+++ GraphHopperGeocoderExample/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 import GraphHopperGeocoder
 
 class ViewController: UIViewController {
@@ -17,7 +18,15 @@ class ViewController: UIViewController {
         let accessToken = "GRAPHHOPPER_ACCESS_TOKEN"
         let geocoder = Geocoder(accessToken: accessToken)
         let query = "Berlin"
-        let options = GeocodeOptions(query)
+        let lat = CLLocationDegrees(exactly: 45.93272)
+        let lng = CLLocationDegrees(exactly: 11.58803)
+        let location = CLLocation(latitude: lat!, longitude: lng!)
+        let point = CLLocationCoordinate2D(latitude: lat!, longitude: lng!)
+
+        var options = GeocodeOptions(query)
+        options = GeocodeOptions("Berlin", point: point)
+        options = GeocodeOptions("Berlin", location: location)
+
         _ = geocoder.calculate(options, completionHandler: { (placemarks, error) in
             placemarks?.forEach({
                 print("\($0.point.latitude) \($0.point.longitude)")
