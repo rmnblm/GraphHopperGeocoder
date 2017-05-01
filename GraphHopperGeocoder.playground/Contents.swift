916cb92fb3e4e679b16cbfbdeb5dc52717f95572
Roman Blum <rmnblm@gmail.com>
2017-05-01 14:01:07 +0200

    :sparkles: replace iOS example with Playground

---
+++ GraphHopperGeocoder.playground/Contents.swift
@@ -0,0 +1,30 @@
+import CoreLocation
+import GraphHopperGeocoder
+import PlaygroundSupport
+
+let accessToken = "GRAPHHOPPER_ACCESS_TOKEN"
+let geocoder = Geocoder(accessToken: accessToken)
+
+let query = "Hotel Strela"
+let forwardGeocodeOptions = ForwardGeocodeOptions(query: query)
+
+_ = geocoder.geocode(forwardGeocodeOptions, completionHandler: { (placemarks, error) in
+    placemarks?.forEach({
+        print("\n")
+        print("Point: \t\t\($0.coordinate.latitude),\($0.coordinate.longitude)")
+        print("OSM ID: \t\t\($0.osmId ?? 0)")
+        print("OSM Type: \t\($0.osmType ?? "")")
+        print("OSM Key: \t\($0.osmKey ?? "")")
+        print("OSM Value: \t\($0.osmValue ?? "")")
+        print("Name: \t\t\($0.name ?? "")")
+        print("Address: \t\($0.postalAddress?.street ?? "")")
+        print("City: \t\t\($0.postalAddress?.postalCode ?? "") \($0.postalAddress?.city ?? "")")
+        print("State: \t\t\($0.postalAddress?.state ?? "")")
+        print("Country: \t\($0.postalAddress?.country ?? "")")
+        if let region = $0.region {
+            print("Region: \t\tNorthWest: \(region[0].latitude) \(region[0].longitude) \tSouthEast: \(region[1].latitude) \(region[1].longitude)")
+        }
+    })
+})
+
+PlaygroundPage.current.needsIndefiniteExecution = true
