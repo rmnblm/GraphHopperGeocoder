dc21553f284b26ee4c37dd6da1560ae6653c9cfb
Phil Schilter <phil.schilter@gmail.com>
2017-05-01 10:46:18 +0200

    :art: placemark improvements

--- GraphHopperGeocoder/GHPlacemark.swift
+++ GraphHopperGeocoder/GHPlacemark.swift
@@ -2,25 +2,17 @@ import CoreLocation
 import Contacts
 
 open class Placemark {
-    public let coordinate: CLLocationCoordinate2D
-    public var osmId: Int?
-    public var osmType: String?
-    public var osmKey: String?
-    public var osmValue: String?
-    public var name: String?
-    public var housenumber: String?
-    public var street: String?
-    public var postalCode: String?
-    public var city: String?
-    public var state: String?
-    public var country: String?
-    public var postalAddress: CNPostalAddress?
+    fileprivate let json: JSONDictionary
 
-    internal init(_ coordinate: CLLocationCoordinate2D) {
-        self.coordinate = coordinate
+    internal init(json: JSONDictionary) {
+        self.json = json
     }
 
-    public convenience init?(json: JSONDictionary) {
+    public convenience init(placemark: Placemark) {
+        self.init(json: placemark.json)
+    }
+
+    open var coordinate: CLLocationCoordinate2D {
         var coordinate = CLLocationCoordinate2D()
 
         if let pointJson = json["point"] as? JSONDictionary {
@@ -30,48 +22,80 @@ open class Placemark {
             }
         }
 
-        self.init(coordinate)
+        return coordinate
+    }
 
-        osmId = json["osm_id"] as? Int
-        osmType = json["osm_type"] as? String
-        osmKey = json["osm_key"] as? String
-        osmValue = json["osm_value"] as? String
-        name = json["name"] as? String
-        housenumber = json["housenumber"] as? String
-        street = json["street"] as? String
-        postalCode = json["postcode"] as? String
-        city = json["city"] as? String
-        state = json["state"] as? String
-        country = json["country"] as? String
+    open var osmId: Int? {
+        return json["osm_id"] as? Int
+    }
 
-        var postalAddress: CNPostalAddress? {
-            let postalAddress = CNMutablePostalAddress()
+    open var osmType: String? {
+        return json["osm_type"] as? String
+    }
 
-            if let street = json["street"] as? String {
-                postalAddress.street = street
+    open var osmKey: String? {
+        return json["osm_key"] as? String
+    }
 
-                if let housenumber = json["housenumber"] as? String {
-                    postalAddress.street = "\(street) \(housenumber)"
-                }
+    open var osmValue: String? {
+        return json["osm_value"] as? String
+    }
+
+    open var name: String? {
+        return json["name"] as? String
+    }
+
+    open var housenumber: String? {
+        return json["housenumber"] as? String
+    }
+
+    open var street: String? {
+        return json["street"] as? String
+    }
+
+    open var postalCode: String? {
+        return json["postcode"] as? String
+    }
+
+    open var city: String? {
+        return json["city"] as? String
+    }
+
+    open var state: String? {
+        return json["state"] as? String
+    }
+
+    open var country: String? {
+        return json["country"] as? String
+    }
+
+    open var postalAddress: CNPostalAddress? {
+        let postalAddress = CNMutablePostalAddress()
+
+        if let street = json["street"] as? String {
+            postalAddress.street = street
+
+            if let housenumber = json["housenumber"] as? String {
+                postalAddress.street = "\(street) \(housenumber)"
             }
-
-            if let city = json["city"] as? String {
-                postalAddress.city = city
-            }
-
-            if let state = json["state"] as? String {
-                postalAddress.state = state
-            }
-
-            if let postalCode = json["postcode"] as? String {
-                postalAddress.postalCode = postalCode
-            }
-
-            if let country = json["country"] as? String {
-                postalAddress.country = country
-            }
-
-            return postalAddress
         }
+
+        if let city = json["city"] as? String {
+            postalAddress.city = city
+        }
+
+        if let state = json["state"] as? String {
+            postalAddress.state = state
+        }
+
+        if let postalCode = json["postcode"] as? String {
+            postalAddress.postalCode = postalCode
+        }
+
+        if let country = json["country"] as? String {
+            postalAddress.country = country
+        }
+
+        return postalAddress
     }
 }
